19a1b659973ded2a28551068cd2f6559cdf71095
Sagar Chand <chandsagar314@gmail.com>
2024-08-07 01:33:05 +0545

    changes in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-import UserButton from "@/components/UserButton";
 import SearchField from "@/components/SearchField";
+import UserButton from "@/components/UserButton";
 
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-10 bg-card shadow-sm">
-      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
+      <div className="mx-auto flex-center max-w-7xl flex-wrap gap-5 px-1 sm:px-5 py-3">
         <Link href="/" className="text-2xl font-bold text-primary">
           bugbook
         </Link>
