6137e667456db8ba05002f40eca26654623a0fda
Sagar Chand <chandsagar314@gmail.com>
2024-08-07 21:30:02 +0545

    created post component and loaded data

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,22 @@
 import PostEditor from "@/components/post/editor/PostEditor";
+import Posts from "@/components/post/Posts";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/types";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
+
   return (
-    <div className="h-[200vh] w-full bg-red-50">
-      <div className="w-full">
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
         <PostEditor />
+        {posts.map((post) => (
+          <Posts key={post.id} post={post} />
+        ))}
       </div>
-    </div>
+    </main>
   );
 }
