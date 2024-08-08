238c378bee8a9249b90babe7d95b4940a5eb1b0b
Sagar Chand <chandsagar314@gmail.com>
2024-08-08 23:16:48 +0545

    following feed completed

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,13 +1,26 @@
 import ForYouFeed from "@/components/post/ForYouFeed";
 import PostEditor from "@/components/post/editor/PostEditor";
 import TrendingSidebar from "@/components/TrendingSidebar";
+import FollowingFeed from "@/components/post/FollowingFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default function Home() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        <ForYouFeed />
+        <Tabs defaultValue="for-you">
+          <TabsList>
+            <TabsTrigger value="for-you">For you</TabsTrigger>
+            <TabsTrigger value="following">Following</TabsTrigger>
+          </TabsList>
+          <TabsContent value="for-you">
+            <ForYouFeed />
+          </TabsContent>
+          <TabsContent value="following">
+            <FollowingFeed />
+          </TabsContent>
+        </Tabs>
       </div>
       <TrendingSidebar />
     </main>
