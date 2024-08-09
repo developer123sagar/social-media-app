b0e8f2770d6ba995e403f1d7dcc610c7056f0caf
Sagar Chand <chandsagar314@gmail.com>
2024-08-10 00:06:27 +0545

    solved build error in uploadthing route

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,9 +1,9 @@
-import { createRouteHandler } from "uploadthing/server";
+import { createRouteHandler } from "uploadthing/next";
 
 import { fileRouter } from "./core";
 
 export const { GET, POST} = createRouteHandler({
     router: fileRouter,
-    
+
 
 })
