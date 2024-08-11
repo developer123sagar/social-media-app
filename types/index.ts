b692ee6014bc40178cfc7ae5463feecc2308202c
Sagar Chand <chandsagar314@gmail.com>
2024-08-11 19:18:05 +0545

    created type for notification and route to fetch all notifications

--- types/index.ts
+++ types/index.ts
@@ -87,6 +87,34 @@ export interface CommentsPage {
   previousCursor: string | null;
 }
 
+export const notificationsInclude = {
+  issuer: {
+    select: {
+      username: true,
+      displayName: true,
+      avatarUrl: true,
+    },
+  },
+  post: {
+    select: {
+      content: true,
+    },
+  },
+} satisfies Prisma.NotificationInclude;
+
+
+
+export type NotificationData = Prisma.NotificationGetPayload<{
+  include: typeof notificationsInclude;
+}>;
+
+
+export interface NotificationsPage {
+  notifications: NotificationData[];
+  nextCursor: string | null;
+}
+
+
 
 export interface FollowerInfo {
   followers: number;
@@ -99,4 +127,8 @@ export interface LikeInfo {
 }
 export interface BookmarkInfo {
   isBookmarkedByUser: boolean;
+}
+
+export interface NotificationCountInfo {
+  unreadCount: number;
 }
