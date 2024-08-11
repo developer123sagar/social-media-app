2720c3b6c12add168792137e7c12148f238a6bf8
Sagar Chand <chandsagar314@gmail.com>
2024-08-11 18:39:38 +0545

    padding changes in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ import UserButton from "@/components/UserButton";
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-10 bg-card shadow-sm">
-      <div className="mx-auto flex-center max-w-7xl flex-wrap gap-5 px-1 sm:px-5 py-3">
+      <div className="mx-auto flex-center max-w-7xl gap-5 px-4 sm:px-5 py-3">
         <Link href="/" className="text-xl sm:text-2xl font-bold text-primary">
           Twittee
         </Link>
